4e4bf3978de1811433e92f31f4ef0c1da44d64c6
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-05-28 05:48:12 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/Login";
+import Browse from "./components/Browse";
 
 const App = () => {
-  return <div className="bg-orange-400 text-9xl text-center">App</div>;
+  const appRouter = createBrowserRouter([
+    { path: "/", element: <Login /> },
+    { path: "/browse", element: <Browse /> },
+  ]);
+  return <RouterProvider router={appRouter} />;
 };
 
 export default App;
